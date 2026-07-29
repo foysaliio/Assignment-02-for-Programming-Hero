94fcb1ca72cfc21a494414adb09d8c5a49627b2a
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-29 20:11:24 +0600

    feat: solve elevator weight safety checker

---
+++ 02 Elevator Weight Safety Checker/isElevatorSafe.js
@@ -0,0 +1,21 @@
+function isElevatorSafe(weights) {
+  if (!Array.isArray(weights)) {
+    return "Invalid";
+  }
+
+  let total = 0;
+  for (const weight of weights) {
+    total += weight;
+  }
+
+  if (total <= 400) {
+    return true;
+  } else if (total > 400) {
+    return false;
+  }
+}
+
+console.log(isElevatorSafe([60, 75, 50])); // true;
+console.log(isElevatorSafe([90, 100, 95, 120])); // false;
+console.log(isElevatorSafe([400])); // true;
+console.log(isElevatorSafe("60,75,50")); // Invalid;
